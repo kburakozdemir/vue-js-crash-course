2694d498eec4436b80651250bbf45ade9275f264
K. Burak Ozdemir <kburakozdemir@gmail.com>
2020-11-01 01:24:02 +0300

    Add Todo data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    {{ msg }}
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true,
+        },
+
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
