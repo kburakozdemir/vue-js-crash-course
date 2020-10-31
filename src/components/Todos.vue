ef95270c1757c9dd2b3c9bade416b217cbd1826e
K. Burak Ozdemir <kburakozdemir@gmail.com>
2020-11-01 01:47:44 +0300

    Add completed checkbox

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <div v-for="todo in todos" v-bind:key="todo">
-        <h3>{{todo.title}}</h3>
+      <TodoItem v-bind:todo="todo" />
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem";
+
 export default {
   name: "Todos",
+  components: {
+    TodoItem,
+  },
   props: ["todos"],
 };
 </script>
