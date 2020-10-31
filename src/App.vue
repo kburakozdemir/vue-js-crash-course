9a66cf3517b5584cf9f0f1536d0eb4dd68537412
K. Burak Ozdemir <kburakozdemir@gmail.com>
2020-11-01 02:48:50 +0300

    Add data from jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
+import axios from "axios";
 
 export default {
   name: "App",
@@ -20,25 +21,14 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true,
-        },
-
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false,
-        },
-      ],
+      todos: [],
     };
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((resp) => (this.todos = resp.data))
+      .catch((err) => console.log(err));
   },
   methods: {
     deleteTodo(id) {
